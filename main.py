2b8ef1b0b1f5cce7c64781b5bb3481ad309a619e
geknusigni <geknusigni@gmail.com>
2025-09-22 10:55:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ json_payload = {
   "install_id": "169c3c4843de5925",
   "app_version": "2.5.13.2025"
 }
-r = curl_cffi.post("https://httpbin.org/post", json=json_payload, impersonate="chrome_android")
-
+r = curl_cffi.post("https://www.theinformation.com/api/v1/login", json=json_payload, impersonate="chrome_android")
+rjson = r.json()
+jwt = rjson["jwt"]
+headers = {
+  "Authorization": f"Bearer {jwt}"
+}
+r = curl_cffi.get("https://www.theinformation.com/api/v1/briefings?per_page=20&page=1&order=feed", headers=headers, impersonate="chrome")
 print(r.json())
